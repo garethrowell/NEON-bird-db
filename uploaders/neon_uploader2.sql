a070db3ae81b9230f547fe1307bce307123159eb
Gareth A Rowell <gar417@gmail.com>
2026-09-20 19:39:12 -0500

    Add files via upload

---
+++ uploaders/neon_uploader2.sql
@@ -0,0 +1,46 @@
+
+drop table if exists dbo.birdobs;
+
+create or replace function neon_uploader2(filepath text)
+returns void as $$
+begin
+        execute	'create table dbo.birdobs(
+	uid text, namedLocation text, domainID text,
+	siteID text, plotID text, plotType text,
+	pointID integer, startDate text,
+	boutNumber integer, eventID text, 
+	pointCountMinute integer, targetTaxaPresent text, 
+	taxonID text, scientificName text, taxonRank text, 
+	vernacularName text,
+	observerDistance real, detectionMethod text, 
+	visualConfirmation text, sexOrAge text, 
+	clusterSize integer, clusterCode text, 
+	identifiedBy text, identificationHistoryID text)';
+        execute format('COPY dbo.birdobs from %s
+		with (FORMAT csv, HEADER)', quote_literal,
+		quote_literal(filepath));
+end;
+$$ language plpgsql;
+
+select neon_uploader2;
+
+
+
+
+
+
+
+
+
+\copy dbo.birdobs(
+        uid, namedLocation, domainID,
+        siteID, plotID, plotType, pointID,
+        startDate, boutNumber, eventID,
+        pointCountMinute, targetTaxaPresent, taxonID,
+        scientificName, taxonRank, vernacularName,
+        observerDistance, detectionMethod, visualConfirmation,
+        sexOrAge, clusterSize, clusterCode,
+        identifiedBy, identificationHistoryID
+)
+from '~/src/csv/Konza2017bird.csv'
+with (FORMAT csv, HEADER true);
